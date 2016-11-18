84734d6f958df8ad95c352ddd7c5ce6f69996138
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-11-19 09:40:06 +1000

    static

--- swe2016/settings.py
+++ swe2016/settings.py
@@ -100,8 +100,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATIC_URL = '/static/'
 if ls.DEV:
-    STATIC_URL = '/static/'
     STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'static')
     ]
