9b5e9bf8b6e9a0dd9762c919a4b8a86295fb2a3f
Zerong Tony Wang <ztony_wang@hotmail.com>
2016-11-19 12:38:48 +1000

    added db

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url
-from .views import IndexView
+from .views import IndexView, LandingView
 
 urlpatterns = [
-    url(r'^$', IndexView.as_view(), name='index')
+    url(r'^$', LandingView.as_view(), name='landing'),
+    url(r'^home/?$', IndexView.as_view(), name='index'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,8 @@ from django.views import View
 class IndexView(View):
     def get(self, request, *args, **kwargs):
         return render(request, 'index.html', {})
+
+
+class LandingView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'landing.html', {})
